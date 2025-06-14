431173fb39d7a88806079e1a7a08a61ba1b24a37
Andrew Brown <ab36245@gmail.com>
2025-06-14 15:35:22 +1000

    Added support for maps

--- lib/src/decoder.dart
+++ lib/src/decoder.dart
@@ -6,6 +6,20 @@ abstract class ModelArrayDecoder {
   ModelArrayDecoder getArray();
   DateTime getDate();
   int getInt();
+  ModelMapDecoder getMap();
+  ModelObjectDecoder getObject();
+  ModelRef getRef();
+  String getString();
+}
+
+abstract class ModelMapDecoder {
+  int get length;
+
+  ModelArrayDecoder getArray();
+  DateTime getDate();
+  int getInt();
+  String getKey();
+  ModelMapDecoder getMap();
   ModelObjectDecoder getObject();
   ModelRef getRef();
   String getString();
@@ -15,6 +29,7 @@ abstract class ModelObjectDecoder {
   ModelArrayDecoder getArray(String name);
   DateTime getDate(String name);
   int getInt(String name);
+  ModelMapDecoder getMap(String name);
   ModelObjectDecoder getObject(String name);
   ModelRef getRef(String name);
   String getString(String name);
--- lib/src/encoder.dart
+++ lib/src/encoder.dart
@@ -4,6 +4,18 @@ abstract class ModelArrayEncoder {
   void putArray(int length, Function(ModelArrayEncoder) handler);
   void putDate(DateTime value);
   void putInt(int value);
+  void putMap(int length, Function(ModelMapEncoder) handler);
+  void putObject(Function(ModelObjectEncoder) handler);
+  void putRef(ModelRef value);
+  void putString(String value);
+}
+
+abstract class ModelMapEncoder {
+  void putArray(int length, Function(ModelArrayEncoder) handler);
+  void putDate(DateTime value);
+  void putInt(int value);
+  void putKey(String value);
+  void putMap(int length, Function(ModelMapEncoder) handler);
   void putObject(Function(ModelObjectEncoder) handler);
   void putRef(ModelRef value);
   void putString(String value);
@@ -13,6 +25,7 @@ abstract class ModelObjectEncoder {
   void putArray(String name, int length, Function(ModelArrayEncoder) handler);
   void putDate(String name, DateTime value);
   void putInt(String name, int value);
+  void putMap(String name, int length, Function(ModelMapEncoder) handler);
   void putObject(String name, Function(ModelObjectEncoder) handler);
   void putRef(String name, ModelRef value);
   void putString(String name, String value);
