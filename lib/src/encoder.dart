1c056316101903f0ae071c6aa046bfaad0021253
Andrew Brown <ab36245@gmail.com>
2025-06-16 15:06:52 +1000

    Refactored encoder to simplify code generation

--- lib/src/encoder.dart
+++ lib/src/encoder.dart
@@ -1,32 +1,35 @@
 import 'ref.dart';
 
 abstract class ModelArrayEncoder {
-  void putArray(int length, Function(ModelArrayEncoder) handler);
+  int get length;
+
+  ModelArrayEncoder putArray(int length);
   void putDate(DateTime value);
   void putInt(int value);
-  void putMap(int length, Function(ModelMapEncoder) handler);
-  void putObject(Function(ModelObjectEncoder) handler);
+  ModelMapEncoder putMap(int length);
+  ModelObjectEncoder putObject();
   void putRef(ModelRef value);
   void putString(String value);
 }
 
 abstract class ModelMapEncoder {
-  void putArray(int length, Function(ModelArrayEncoder) handler);
+  int get length;
+
+  ModelArrayEncoder putArray(int length);
   void putDate(DateTime value);
   void putInt(int value);
-  void putKey(String value);
-  void putMap(int length, Function(ModelMapEncoder) handler);
-  void putObject(Function(ModelObjectEncoder) handler);
+  ModelMapEncoder putMap(int length);
+  ModelObjectEncoder putObject();
   void putRef(ModelRef value);
   void putString(String value);
 }
 
 abstract class ModelObjectEncoder {
-  void putArray(String name, int length, Function(ModelArrayEncoder) handler);
+  ModelArrayEncoder putArray(String name, int length);
   void putDate(String name, DateTime value);
   void putInt(String name, int value);
-  void putMap(String name, int length, Function(ModelMapEncoder) handler);
-  void putObject(String name, Function(ModelObjectEncoder) handler);
+  ModelMapEncoder putMap(String name, int length);
+  ModelObjectEncoder putObject(String name);
   void putRef(String name, ModelRef value);
   void putString(String name, String value);
 }
