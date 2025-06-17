66ef6574db09d1268507ae333061e0561752ff94
Andrew Brown <ab36245@gmail.com>
2025-06-17 15:25:40 +1000

    Added bool & float and changed date to time

--- lib/src/decoder.dart
+++ lib/src/decoder.dart
@@ -4,33 +4,39 @@ abstract class ModelArrayDecoder {
   int get length;
 
   ModelArrayDecoder getArray();
-  DateTime getDate();
+  bool getBool();
+  double getFloat();
   int getInt();
   ModelMapDecoder getMap();
   ModelObjectDecoder getObject();
   ModelRef getRef();
   String getString();
+  DateTime getTime();
 }
 
 abstract class ModelMapDecoder {
   int get length;
 
   ModelArrayDecoder getArray();
-  DateTime getDate();
+  bool getBool();
+  double getFloat();
   int getInt();
   String getKey();
   ModelMapDecoder getMap();
   ModelObjectDecoder getObject();
   ModelRef getRef();
   String getString();
+  DateTime getTime();
 }
 
 abstract class ModelObjectDecoder {
   ModelArrayDecoder getArray(String name);
-  DateTime getDate(String name);
+  bool getBool(String name);
+  double getFloat(String name);
   int getInt(String name);
   ModelMapDecoder getMap(String name);
   ModelObjectDecoder getObject(String name);
   ModelRef getRef(String name);
   String getString(String name);
+  DateTime getTime(String name);
 }
--- lib/src/encoder.dart
+++ lib/src/encoder.dart
@@ -4,32 +4,38 @@ abstract class ModelArrayEncoder {
   int get length;
 
   ModelArrayEncoder putArray(int length);
-  void putDate(DateTime value);
+  void putBool(bool value);
+  void putFloat(double value);
   void putInt(int value);
   ModelMapEncoder putMap(int length);
   ModelObjectEncoder putObject();
   void putRef(ModelRef value);
   void putString(String value);
+  void putTime(DateTime value);
 }
 
 abstract class ModelMapEncoder {
   int get length;
 
   ModelArrayEncoder putArray(int length);
-  void putDate(DateTime value);
+  void putBool(bool value);
+  void putFloat(double value);
   void putInt(int value);
   ModelMapEncoder putMap(int length);
   ModelObjectEncoder putObject();
   void putRef(ModelRef value);
   void putString(String value);
+  void putTime(DateTime value);
 }
 
 abstract class ModelObjectEncoder {
   ModelArrayEncoder putArray(String name, int length);
-  void putDate(String name, DateTime value);
+  void putBool(String name, bool value);
+  void putFloat(String name, double value);
   void putInt(String name, int value);
   ModelMapEncoder putMap(String name, int length);
   ModelObjectEncoder putObject(String name);
   void putRef(String name, ModelRef value);
   void putString(String name, String value);
+  void putTime(String name, DateTime value);
 }
