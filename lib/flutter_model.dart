ba89f8a7c88f3ca9f04a52d586aedece5c2de111
Andrew Brown <ab36245@gmail.com>
2025-06-19 15:17:09 +1000

    Exported Uint8List and ObjectWriter for convenience

--- lib/flutter_model.dart
+++ lib/flutter_model.dart
@@ -1,3 +1,6 @@
+export 'dart:typed_data' show Uint8List;
+export 'package:flutter_writer/flutter_writer.dart' show ObjectWriter;
+
 export 'src/codec.dart';
 export 'src/decoder.dart';
 export 'src/encoder.dart';
